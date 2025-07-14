c36e067efdd673bf4e16cfd9561549745b006a51
DanielSason <danielsason555@gmail.com>
2025-07-14 23:37:46 +0300

    implemented PagingManager.find_free_pages() that finds contiguous virtual pages for PagingManager.alloacte()

--- kernel/include/kernel/memory/PagingManager.h
+++ kernel/include/kernel/memory/PagingManager.h
@@ -5,6 +5,9 @@
 extern "C" void loadPageDirectory(unsigned int*);
 extern "C" void enablePaging();
 
+#define PAGE_DIRECTORY_ENTRIES 1024
+#define PAGE_TABLE_ENTRIES 1024
+
 
 /* 4-MiB (large) page entry — valid when PS=1 in PDE             *
  * Layout from Intel SDM Vol.3, Table 4-13 (32-bit paging).       */
@@ -22,11 +25,19 @@ typedef union __attribute__((packed))
 		uint32_t accessed : 1;		// bit 5  (A)
 		uint32_t dirty : 1;			// bit 6  (D) – set by CPU on write
 		uint32_t page_size : 1;		// bit 7  (PS=1 for big page)
-		uint32_t global : 1;		// bit 8  (G)
-		uint32_t avail : 3;			// bits 9-11 (OS-defined)
-		uint32_t pat : 1;			// bit 12 (PAT index bit 2)
-		uint32_t reserved : 9;		// bits 13-21 (must be 0)
-		uint32_t phys_addr : 10;	// bits 22-31 -> bits 32-41 of frame
+		union {
+			struct {
+				uint32_t global : 1;		// bit 8  (G)
+				uint32_t avail : 3;			// bits 9-11 (OS-defined)
+				uint32_t pat : 1;			// bit 12 (PAT index bit 2)
+				uint32_t reserved : 9;		// bits 13-21 (must be 0)
+				uint32_t phys_addr : 10;	// bits 22-31 -> bits 32-41 of frame
+			} goliath;
+			struct {
+				uint32_t avail : 4;			// bits 9-11 (OS-defined)
+				uint32_t phys_addr : 20;	// bits 22-31 -> bits 32-41 of frame
+			} david;
+		} page_kind;
 	} bits;
 } PageDirectoryEntry;
 
@@ -65,4 +76,8 @@ public:
 	{
 		loadPageDirectory((unsigned int *)pageDirectory);
 	}
+	PageTableEntry* find_free_pages(size_t num);
+	
+	void* allocate(size_t size);
+	
 };
--- kernel/kernel/memory/PagingManager.cpp
+++ kernel/kernel/memory/PagingManager.cpp
@@ -1,26 +1,83 @@
 #include <kernel/kernel.h>
 #include <kernel/memory/PagingManager.h>
 
-
-
-
 void PagingManager::init()
 {
-
 	pageDirectory = (PageDirectoryEntry *)krn.memoryManager.buddy_allocator.allocate(BUDDY_ALLOCATOR_MIN_BLOCK_SIZE);
 	if (pageDirectory == nullptr)
 	{
-        printf("you murdered the kernel :(\n");
+		printf("you murdered the kernel :(\n");
 	}
+	for (size_t i = 0; i < PAGE_DIRECTORY_ENTRIES; i++)
+	{
+		pageDirectory[i].value = 0;
+	}
+
+	// TODO: delete pagetable allocations from init
 	PageTableEntry *pageTable = (PageTableEntry *)krn.memoryManager.buddy_allocator.allocate(BUDDY_ALLOCATOR_MIN_BLOCK_SIZE);
-	for (int i = 0; i < 1024; i++)
+	for (size_t i = 0; i < PAGE_TABLE_ENTRIES; i++)
 	{
 		// As the address is page aligned, it will always leave 12 bits zeroed.
 		// Those bits are used by the attributes ;)
 		pageTable[i].value = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
 	}
-	pageDirectory[0] = PageDirectoryEntry {.value = (uint32_t)pageTable | 3};
-
-
+	pageDirectory[0] = PageDirectoryEntry{.value = (uint32_t)pageTable | 3};
 }
 
+size_t find_free_pages(size_t num, size_t *dir_entry, size_t *table_entry)
+{
+	size_t count = 0;
+	for (size_t i = 0; i < PAGE_DIRECTORY_ENTRIES; i++)
+	{
+		PageTableEntry *pageTable = (PageTableEntry *)(pageDirectory[i].bits.page_kind.david.phys_addr << 12);
+		for (size_t j = 0; j < PAGE_TABLE_ENTRIES; j++)
+		{
+			if (pageTable[j].bits.present)
+			{
+				if (++count == num)
+				{
+					*table_entry = j - count % PAGE_TABLE_ENTRIES + 1;
+					*dir_entry = i - count / PAGE_TABLE_ENTRIES;
+					if (j < num)
+						*dir_entry--;
+					if (table_entry < 0)
+						*table_entry += PAGE_TABLE_ENTRIES;
+					// PageTableEntry *page_target = (PageTableEntry *)(pageDirectory[dir_entry].bits.page_kind.david.phys_addr << 12);
+					return num;
+				}
+			}
+			else
+			{
+				count = 0;
+			}
+		}
+	}
+	return 0;
+}
+
+void *allocate(size_t size)
+{
+	size_t pages_num = (size + BLOCK_SIZE - 1) >> 12;
+	size_t dir_entry, table_entry;
+	size_t new_address = find_free_pages(size, &dir_entry, &table_entry);
+	if (new_address == 0)
+		return nullptr;
+	
+	size_t i = 0;
+	for (; i < pages_num; i++)
+	{
+		void *ptr = krn.memoryManager.buddy_allocator.allocate(BUDDY_ALLOCATOR_MIN_BLOCK_SIZE);
+		if (ptr == nullptr)
+		 	break;
+
+		PageTableEntry *page = (PageTableEntry *)(pageDirectory[dir_entry].bits.page_kind.david.phys_addr << 12);
+		page[table_entry + i] = PageTableEntry{.value = (uint32_t)ptr | 3};
+		
+		// TODO: implement
+	}
+
+	// find size/4k free entries
+
+	// map the physical address to a virtual address
+	return new_address;
+}
